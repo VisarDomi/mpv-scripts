c46320311da6f96afa40b4753d268cf5b6414962
Visar Domi <visardomi4@gmail.com>
2023-12-08 01:40:19 +0100

    .

--- visualizer.lua
+++ visualizer.lua
@@ -14,7 +14,12 @@ mp.add_key_binding("Ctrl+b", "cycle-visualizer", function()
 
         local lavfi = "[aid1] asplit [ao][a];" ..
                     "[a] afifo, aformat=channel_layouts=stereo, " ..
-                    "firequalizer=gain='if(gte(f,200),10,0)':scale=linlog [a_eq];" ..
+                    "firequalizer       =" ..
+                        "gain           = '1.4884e8 * f*f*f / (f*f + 424.36) / (f*f + 1.4884e8) / sqrt(f*f + 25122.25)':" ..
+                        "scale          = linlin:" ..
+                        "wfunc          = tukey:" ..
+                        "zero_phase     = on:" ..
+                        "fft2           = on [a_eq];" ..
                     "[a_eq] showcqt=s=" .. width .. "x" .. height .. ", format=rgba, colorkey=black [v];" ..
                     "[vid1][v] overlay=format=auto [vo]"
         mp.set_property("options/lavfi-complex", lavfi)
